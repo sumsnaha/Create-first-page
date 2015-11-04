5e6b8885d28174425e10897d1e17e03446c811b5
snahar <snahar@usgbc.org>
2015-11-04 15:48:02 -0500

    Added server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
 
-app.get('/index.json', function(req, res){
-	fs.readFile( __dirname + "/" + "index.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello Sumsun,');
 });
 
-	
-  var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
