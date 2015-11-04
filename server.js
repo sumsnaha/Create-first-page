583fe09f888413c3f304b81e28129ec059ec6883
snahar <snahar@usgbc.org>
2015-11-04 11:59:27 -0500

    Added server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
-	res.send('Hello Sumsun');
+	res.send('Hello Sumsun, your website is working');
 });
 
 app.listen(3000);
