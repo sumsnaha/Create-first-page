70bcf4980f3f3f42cf7c871214524569f7db712a
snahar <snahar@usgbc.org>
2015-11-04 11:26:26 -0500

    Added index.html

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var s = http.createServer(function(req,res)
 {res.writeHead(200, { 'content=type' : 'text/plain' });
     res.end("Hello World\n");
 
-}).listen(1337, '127.0.0.1');
-console.log('sever running at http://preview.i9jupc6ek10dx6r0sweafqimhsvpldi320qg47b76bqehfr.box.codeanywhere.com:1337/');
+}).listen(3000, 'preview.i9jupc6ek10dx6r0sweafqimhsvpldi320qg47b76bqehfr.box.codeanywhere.com');
+console.log('sever running at http://preview.i9jupc6ek10dx6r0sweafqimhsvpldi320qg47b76bqehfr.box.codeanywhere.com:3000/');
