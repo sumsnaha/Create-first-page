ac8fd22f52e5ddac753fcda3d89d417507aee9f6
snahar <snahar@usgbc.org>
2015-11-04 11:03:36 -0500

    Added index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-/* Hello, World! program in node.js with github */
-console.log("Hello, World!");
+var http = require('http');
+var s = http.createServer(function(req,res)
+{res.writeHead(200, { 'content=type' : 'text/plain' });
+    res.end("Hello World\n");
+
+}).listen(1337, '127.0.0.1');
+console.log('sever running at http://preview.i9jupc6ek10dx6r0sweafqimhsvpldi320qg47b76bqehfr.box.codeanywhere.com:1337/');
